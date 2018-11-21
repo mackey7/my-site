cb5ee0a11310d183b1d6405051169cc4611914d5
Maciej Bogdanski <m.bogdanskii@gmail.com>
2018-11-21 18:34:37 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('app/scss/style.scss').pipe(sass()) // Converts Sass to CSS with gulp-sass
+        .pipe(gulp.dest('app/css'))
+}
+
+);
+
+
+gulp.task('watch', function () {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    // Other watchers
+})
